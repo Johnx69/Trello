2f3a69dd020f98a3eeaa57c11a3eab45d2bcae5e
Johnx69 <hoanganh692004@gmail.com>
2023-09-03 09:59:30 -0400

    refactor code

---
+++ frontend/src/pages/Boards/BoardContent/ListColumns/Column/ListCards/ListCards.jsx
@@ -0,0 +1,29 @@
+import { Box } from "@mui/material";
+import Card from "./Card/Card";
+
+function ListCards() {
+    return (
+        <Box
+            sx={{
+                p: "0 5px",
+                m: "0 5px",
+                display: "flex",
+                flexDirection: "column",
+                gap: 1,
+                overflowX: "hidden",
+                overflowY: "auto",
+                maxHeight: (theme) =>
+                    `calc(${theme.trello.boardContentHeight} - ${theme.spacing(
+                        5
+                    )} - ${(theme) => theme.trello.columnFooterHeight} - ${(
+                        theme
+                    ) => theme.trello.columnHeaderHeight})`,
+            }}
+        >
+            <Card />
+            <Card temporaryHideMedia/>
+        </Box>
+    );
+}
+
+export default ListCards;
--- frontend/src/pages/Boards/_id.jsx
+++ frontend/src/pages/Boards/_id.jsx
@@ -1,7 +1,7 @@
 import { Container } from "@mui/material";
-import AppBar from "~/components/AppBar";
+import AppBar from "~/components/AppBar/AppBar";
 import BoardBar from "./BoardBar";
-import BoardContent from "./BoardContent";
+import BoardContent from "./BoardContent/BoardContent";
 function Board() {
     return (
         <Container disableGutters maxWidth={false} sx={{ height: "100vh" }}>
