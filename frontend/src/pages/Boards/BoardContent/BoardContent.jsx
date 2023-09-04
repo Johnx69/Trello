03a8c533552b6abccfea473a7513695e7e127616
Johnx69 <hoanganh692004@gmail.com>
2023-09-04 16:09:23 -0400

    drag and drop feature

--- frontend/src/pages/Boards/BoardContent/BoardContent.jsx
+++ frontend/src/pages/Boards/BoardContent/BoardContent.jsx
@@ -1,27 +1,222 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Box } from "@mui/material";
 
 import ListColumns from "./ListColumns/ListColumns";
 import { mapOrder } from "~/utils/sorts";
+import {
+    DndContext,
+    // PointerSensor,
+    MouseSensor,
+    TouchSensor,
+    useSensor,
+    useSensors,
+    DragOverlay,
+    defaultDropAnimationSideEffects,
+    closestCorners,
+} from "@dnd-kit/core";
+import { arrayMove } from "@dnd-kit/sortable";
+
+import Column from "./ListColumns/Column/Column";
+import Card from "./ListColumns/Column/ListCards/Card/Card";
+
+import { cloneDeep } from "lodash";
+
+const ACTIVE_DRAG_ITEM_TYPE = {
+    COLUMN: "ACTIVE_DRAG_ITEM_TYPE_COLUMN",
+    CARD: "ACTIVE_DRAG_ITEM_TYPE_CARD",
+};
 
 function BoardContent({ board }) {
-    const orderedColumns = mapOrder(
-        board?.columns,
-        board?.columnOrderIds,
-        "_id"
-    );
+    // const pointerSensor = useSensor(PointerSensor, {
+    //     activationConstraint: { distance: 10 },
+    // });
+
+    const mouseSensor = useSensor(MouseSensor, {
+        activationConstraint: { distance: 10 },
+    });
+
+    const touchSensor = useSensor(TouchSensor, {
+        activationConstraint: { delay: 250, tolerance: 500 },
+    });
+
+    const sensors = useSensors(mouseSensor, touchSensor);
+
+    const [orderedColumns, setOrderedColumns] = useState([]);
+
+    const [activeDragItemId, setActiveDragItemId] = useState(null);
+    const [activeDragItemType, setActiveDragItemType] = useState(null);
+    const [activeDragItemData, setActiveDragItemData] = useState(null);
+
+    useEffect(() => {
+        setOrderedColumns(
+            mapOrder(board?.columns, board?.columnOrderIds, "_id")
+        );
+    }, [board]);
+
+    const findColumnByCardId = (cardId) => {
+        return orderedColumns.find((column) =>
+            column?.cards?.map((card) => card._id)?.includes(cardId)
+        );
+    };
+
+    const handleDragStart = (event) => {
+        setActiveDragItemId(event?.active?.id);
+        setActiveDragItemType(
+            event?.active?.data?.current?.columnId
+                ? ACTIVE_DRAG_ITEM_TYPE.CARD
+                : ACTIVE_DRAG_ITEM_TYPE.COLUMN
+        );
+        setActiveDragItemData(event?.active?.data?.current);
+    };
+
+    const handleDragOver = (event) => {
+        if (activeDragItemType === ACTIVE_DRAG_ITEM_TYPE.COLUMN) return;
+
+        const { active, over } = event;
+
+        if (!active || !over) return;
+
+        const {
+            id: activeDraggingCardId,
+            data: { current: activeDraggingCardData },
+        } = active;
+        const { id: overCardId } = over;
+
+        const activeColumn = findColumnByCardId(activeDraggingCardId);
+        const overColumn = findColumnByCardId(overCardId);
+
+        if (!activeColumn || !overColumn) return;
+
+        if (activeColumn._id !== overColumn._id) {
+            setOrderedColumns((prevColumns) => {
+                const overCardIndex = overColumn?.cards?.findIndex(
+                    (card) => card._id === overCardId
+                );
+                let newCardIndex;
+                const isBelowOverItem =
+                    active.rect.current.translated &&
+                    active.rect.current.translated.top >
+                        over.rect.top + over.rect.height;
+                const modifier = isBelowOverItem ? 1 : 0;
+
+                newCardIndex =
+                    overCardIndex >= 0
+                        ? overCardIndex + modifier
+                        : overColumn?.cards?.length + 1;
+
+                const nextColumns = cloneDeep(prevColumns);
+
+                const nextActiveColumn = nextColumns.find(
+                    (column) => column._id === activeColumn._id
+                );
+
+                const nextOverColumn = nextColumns.find(
+                    (column) => column._id === overColumn._id
+                );
+
+                if (nextActiveColumn) {
+                    //
+                    nextActiveColumn.cards = nextActiveColumn.cards.filter(
+                        (card) => card._id !== activeDraggingCardId
+                    );
+                    nextActiveColumn.cardOrderIds = nextActiveColumn.cards.map(
+                        (card) => card._id
+                    );
+                }
+
+                if (nextOverColumn) {
+                    //
+                    nextOverColumn.cards = nextOverColumn.cards.filter(
+                        (card) => card._id !== activeDraggingCardId
+                    );
+
+                    nextOverColumn.cards = nextOverColumn.cards.toSpliced(
+                        newCardIndex,
+                        0,
+                        activeDraggingCardData
+                    );
+                    nextOverColumn.cardOrderIds = nextOverColumn.cards.map(
+                        (card) => card._id
+                    );
+                }
+
+                return nextColumns;
+            });
+        }
+    };
+
+    const handleDragEnd = (event) => {
+        if (activeDragItemType === ACTIVE_DRAG_ITEM_TYPE.CARD) {
+            console.log();
+        }
+
+        const { active, over } = event;
+
+        if (!active || !over) return;
+
+        if (active.id !== over.id) {
+            const oldIndex = orderedColumns.findIndex(
+                (c) => c._id === active.id
+            );
+
+            const newIndex = orderedColumns.findIndex((c) => c._id === over.id);
+
+            const dndOrderedColumns = arrayMove(
+                orderedColumns,
+                oldIndex,
+                newIndex
+            );
+
+            // const dndOrderedColumnsIds = dndOrderedColumns.map((c) => c._id);
+            // console.log(dndOrderedColumns);
+            // console.log(dndOrderedColumnsIds);
+
+            setOrderedColumns(dndOrderedColumns);
+            setActiveDragItemId(null);
+            setActiveDragItemData(null);
+            setActiveDragItemType(null);
+        }
+    };
+
+    const customDropAnimation = {
+        sideEffects: defaultDropAnimationSideEffects({
+            styles: {
+                active: {
+                    opacity: "0.5",
+                },
+            },
+        }),
+    };
+
     return (
-        <Box
-            sx={{
-                bgcolor: (theme) =>
-                    theme.palette.mode === "dark" ? "#34495e" : "#1976d2",
-                width: "100%",
-                height: (theme) => theme.trello.boardContentHeight,
-                p: "10px 0",
-            }}
+        <DndContext
+            sensors={sensors}
+            onDragEnd={handleDragEnd}
+            collisionDetection={closestCorners}
+            onDragOver={handleDragOver}
+            onDragStart={handleDragStart}
         >
-            <ListColumns columns={orderedColumns} />
-        </Box>
+            <Box
+                sx={{
+                    bgcolor: (theme) =>
+                        theme.palette.mode === "dark" ? "#34495e" : "#1976d2",
+                    width: "100%",
+                    height: (theme) => theme.trello.boardContentHeight,
+                    p: "10px 0",
+                }}
+            >
+                <ListColumns columns={orderedColumns} />
+                <DragOverlay dropAnimation={customDropAnimation}>
+                    {!activeDragItemType && null}
+                    {activeDragItemType === ACTIVE_DRAG_ITEM_TYPE.COLUMN && (
+                        <Column column={activeDragItemData} />
+                    )}
+                    {activeDragItemType === ACTIVE_DRAG_ITEM_TYPE.CARD && (
+                        <Card card={activeDragItemData} />
+                    )}
+                </DragOverlay>
+            </Box>
+        </DndContext>
     );
 }
 
--- frontend/src/pages/Boards/BoardContent/ListColumns/Column/ListCards/ListCards.jsx
+++ frontend/src/pages/Boards/BoardContent/ListColumns/Column/ListCards/ListCards.jsx
@@ -1,30 +1,38 @@
 import { Box } from "@mui/material";
 import Card from "./Card/Card";
-
+import {
+    SortableContext,
+    verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
 function ListCards({ cards }) {
     return (
-        <Box
-            sx={{
-                p: "0 5px",
-                m: "0 5px",
-                display: "flex",
-                flexDirection: "column",
-                gap: 1,
-                overflowX: "hidden",
-                overflowY: "auto",
-                maxHeight: (theme) =>
-                    `calc(${theme.trello.boardContentHeight} - ${theme.spacing(
-                        5
-                    )} - ${(theme) => theme.trello.columnFooterHeight} - ${(
-                        theme
-                    ) => theme.trello.columnHeaderHeight})`,
-            }}
+        <SortableContext
+            items={cards?.map((c) => c._id)}
+            strategy={verticalListSortingStrategy}
         >
-            {cards?.map((card) => (
-                <Card key={card._id} card={card} />
-            ))}
-            <Card />
-        </Box>
+            <Box
+                sx={{
+                    p: "0 5px",
+                    m: "0 5px",
+                    display: "flex",
+                    flexDirection: "column",
+                    gap: 1,
+                    overflowX: "hidden",
+                    overflowY: "auto",
+                    maxHeight: (theme) =>
+                        `calc(${
+                            theme.trello.boardContentHeight
+                        } - ${theme.spacing(5)} - ${(theme) =>
+                            theme.trello.columnFooterHeight} - ${(theme) =>
+                            theme.trello.columnHeaderHeight})`,
+                }}
+            >
+                {cards?.map((card) => (
+                    <Card key={card._id} card={card} />
+                ))}
+                <Card />
+            </Box>
+        </SortableContext>
     );
 }
 
